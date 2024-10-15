25a705149b547a3dc60323eba52ab3964ac36509
Esteban Rincon <94997831+EstebanRincon12@users.noreply.github.com>
2024-10-15 11:36:28 -0500

    arreglos

--- lib/widgets/add_habit.dart
+++ lib/widgets/add_habit.dart
@@ -1,99 +1,12 @@
-import 'package:evaluacion/widgets/habits.dart';
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
-    const Homepage({super.key});
+class AddHabit extends StatelessWidget {
+  const AddHabit({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final List<Habitos> listHabits=[
-      Habitos(days: 10, name: 'HAbito 1'),
-      Habitos(days: 10, name: 'HAbito 1'),
-      Habitos(days: 10, name: 'HAbito 1'),
-      Habitos(days: 10, name: 'HAbito 1'),
-      Habitos(days: 10, name: 'HAbito 1'),
-    ];
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Añadir a Favoritos'),
-        backgroundColor: Colors.black,
-      ),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(16),
-            margin: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: Colors.white, 
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: const Column(
-              children: [
-                Text(
-                  'Habitos:',
-                  style: TextStyle(
-                    color: Colors.black, 
-                    fontSize: 18,
-                  ),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  'Lista de Habitos',
-                  style: TextStyle(
-                    color: Colors.black, 
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: listHabits.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  margin: const EdgeInsets.all(8),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  color: Colors.yellow,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Row(
-                      children: [
-                        const SizedBox(width: 16),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                listHabits[index].name,
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const SizedBox(height: 4),
-                              Text(
-                                'Meta de dias: ${listHabits[index]..days}',
-                                style: const TextStyle(fontSize: 14),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
+    return Container(
+      
     );
   }
 }
-
-
--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -1,46 +1,101 @@
-import 'package:evaluacion/widgets/add_habit.dart';
+import 'package:evaluacion/widgets/habits.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+    const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final List<Habitos> listHabits=[
+      Habitos(days: 10, name: 'HAbito 1'),
+      Habitos(days: 10, name: 'HAbito 1'),
+      Habitos(days: 10, name: 'HAbito 1'),
+      Habitos(days: 10, name: 'HAbito 1'),
+      Habitos(days: 10, name: 'HAbito 1'),
+    ];
+
     return Scaffold(
-      floatingActionButton: SpeedDial(
-        animatedIcon: AnimatedIcons.menu_close,
+      appBar: AppBar(
+        title: const Text('Añadir a Favoritos'),
         backgroundColor: Colors.black,
+      ),
+      body: Column(
         children: [
-          SpeedDialChild(
-            child: const Icon(Icons.ad_units, color: Colors.white),
-            label: 'Agregar Habito',
-            backgroundColor: Colors.green,
-            onTap: () {
-              showModalBottomSheet(
-                context: context,
-                isScrollControlled:
-                    true, // Permite que el modal ocupe más espacio
-                backgroundColor: Colors.white,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          Container(
+            padding: const EdgeInsets.all(16),
+            margin: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.white, 
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: const Column(
+              children: [
+                Text(
+                  'Habitos:',
+                  style: TextStyle(
+                    color: Colors.black, 
+                    fontSize: 18,
+                  ),
                 ),
-                builder: (BuildContext context) {
-                  return const AddHabit();
-                },
-              );
-            },
+                SizedBox(height: 8),
+                Text(
+                  'Lista de Habitos',
+                  style: TextStyle(
+                    color: Colors.black, 
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
           ),
-          SpeedDialChild(
-            child: const Icon(Icons.favorite_border, color: Colors.white),
-            label: 'Resumen',
-            backgroundColor: Colors.red,
-            onTap: () {
-              print('Eliminar de Favoritos');
-            },
+          Expanded(
+            child: ListView.builder(
+              itemCount: listHabits.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: const EdgeInsets.all(8),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  color: Colors.yellow,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Row(
+                      children: [
+                        const SizedBox(width: 16),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                listHabits[index].name,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 4),
+                              Text(
+                                'Meta de dias: ${listHabits[index]..days}',
+                                style: const TextStyle(fontSize: 14),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
+          
         ],
       ),
+      
     );
   }
 }
+
+
